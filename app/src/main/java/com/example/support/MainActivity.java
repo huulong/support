f903df65135688f31892873c293a693db6575ec3
huulong <longanhminh9@gmail.com>
2023-06-06 01:45:12 +0700

    nhanh2

--- app/src/main/java/com/example/support/MainActivity.java
+++ app/src/main/java/com/example/support/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.support;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button callBtn;
+    Button messageBtn;
+    Button BtnCompany;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callBtn = findViewById(R.id.button1);
+        messageBtn = findViewById(R.id.button2);
+
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0396647951"));
+                startActivityForResult(callIntent, 0);
+            }
+        });
+
+        messageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.me/huulongmedia"));
+                startActivity(intent);
+            }
+        });
+        ImageView imageView = (ImageView) findViewById(R.id.imageView5);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://devlong.id.vn/Info.html"; // Đường dẫn tới trang web cần hiển thị
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+        ImageView imageView4 = (ImageView) findViewById(R.id.imageView4);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://devlong.id.vn/chinhsach.html"; // Đường dẫn tới trang web cần hiển thị
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+        ImageView imageView6 = (ImageView) findViewById(R.id.imageView6);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://devlong.id.vn/tranhchap.html"; // Đường dẫn tới trang web cần hiển thị
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+        ImageView imageView7 = (ImageView) findViewById(R.id.imageView7);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://devlong.id.vn/"; // Đường dẫn tới trang web cần hiển thị
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
     }
 }
